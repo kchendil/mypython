de024b39f7afe939b1c59f18a22fdfa9d7323aae
Chendil <mkchendil@gmail.com>
2019-04-26 08:52:37 +0000

    learnt tupples and sets

---
+++ set.py
@@ -0,0 +1,6 @@
+thisset = {"apple","banana","cherry"}
+print(thisset)
+for x in thisset:
+  print(x)
+if "cherry" in thisset:
+  print("Yes, 'cherry' is in thisset")
---
+++ tupples.py
@@ -0,0 +1,10 @@
+thistuple=("apple","banana","cherry")
+print(thistuple)
+print(thistuple[2])
+for x in thistuple:
+  print (x)
+if "apple" in thistuple:
+  print("Yes, 'apple is in the fruits tuple")
+print(len(thistuple))
+print(thistuple.count("apple"))
+print(thistuple.index("cherry"))
