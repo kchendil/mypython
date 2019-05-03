9179928c86e6e06888bdcbd2fdd26044e4de6a76
Chendil <mkchendil@gmail.com>
2019-05-03 08:24:44 +0000

    python sets continued

--- set.py
+++ set.py
@@ -4,3 +4,10 @@ for x in thisset:
   print(x)
 if "cherry" in thisset:
   print("Yes, 'cherry' is in thisset")
+thisset.add("orange")
+print(thisset)
+thisset.update(["mango","grapes"])
+print(thisset)
+print(len(thisset))
+thisset.remove("grapes")
+print(thisset)
