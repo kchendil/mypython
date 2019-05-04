de267c49d49e7c98758381682e6075fa6f30e79f
Chendil <mkchendil@gmail.com>
2019-05-04 07:32:18 +0000

    added iam role

--- aws_lambda/createEC2Instance.py
+++ aws_lambda/createEC2Instance.py
@@ -6,10 +6,14 @@ def lambda_handler(event, context):
 
     ec2 = boto3.resource('ec2',region_name='ap-south-1')
     instance = ec2.create_instances(
-        ImageId='ami-02e35f37be749452c',
+        ImageId='ami-047f1e700894ee788',
         InstanceType='t2.micro',
         KeyName = 'chendil-key',
         SubnetId = 'subnet-505bdc1c',
+        IamInstanceProfile={
+                            'Arn': 'arn:aws:iam::012016562543:role/Ansible_role'
+                            'Name': 'Ansible_role'
+                     })
         MinCount=1,
         MaxCount=1,
         SecurityGroupIds=[
